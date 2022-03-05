7b47b5a9e65aaec42b1f1a394802659d71a946e3
makuke1234 <makuke1234@gmail.com>
2022-03-05 15:15:42 +0200

    Funktsiooni-nimede mugandus

--- src/fileHelper.h
+++ src/fileHelper.h
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <stdint.h>
 
-char * fhelper_readFile(const char * restrict fileName, intptr_t fnameLength, size_t * restrict resultLength);
-char * fhelper_readFileBinary(const char * restrict fileName, intptr_t fnameLength, size_t * restrict resultLength);
+char * fhelper_read(const char * restrict fileName, intptr_t fnameLength, size_t * restrict resultLength);
+char * fhelper_readBin(const char * restrict fileName, intptr_t fnameLength, size_t * restrict resultLength);
 
-intptr_t fhelper_writeFile(const char * fileName, intptr_t fnameLength, const char * string, intptr_t stringLength);
-intptr_t fhelper_writeFileBinary(const char * fileName, intptr_t fnameLength, const char * string, intptr_t stringLength);
+intptr_t fhelper_write(const char * fileName, intptr_t fnameLength, const char * string, intptr_t stringLength);
+intptr_t fhelper_writeBin(const char * fileName, intptr_t fnameLength, const char * string, intptr_t stringLength);
 
 
 #endif
