fa0208708a278fecda2a1b2345c0979f89f077f9
makuke1234 <makuke1234@gmail.com>
2022-03-10 00:00:26 +0200

    Andmemudeli täiendus

--- src/dataModel.c
+++ src/dataModel.c
@@ -1,6 +1,7 @@
 #include "dataModel.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
 bool point_initStr(point_t * restrict p, const char * restrict idstr, const char * restrict valuestr)
 {
@@ -17,17 +18,85 @@ bool point_initStr(point_t * restrict p, const char * restrict idstr, const char
 
 	return true;
 }
+point_t * point_makeStr(const char * restrict idstr, const char * restrict valuestr)
+{
+	point_t * p = malloc(sizeof(point_t));
+	if (p == NULL)
+	{
+		return NULL;
+	}
+	else if (!point_initStr(p, idstr, valuestr))
+	{
+		free(p);
+		return NULL;
+	}
+
+	return p;
+}
 void point_destroy(point_t * restrict p)
 {
 	assert(p != NULL);
 	iniString_destroy(&p->id);
 }
+void point_free(point_t * restrict p)
+{
+	assert(p != NULL);
 
+	point_destroy(p);
+	free(p);
+}
+
+bool line_initStr(
+	line_t * restrict l,
+	const hashMap_t * restrict pointmap,
+	const char * restrict idstr,
+	const char * restrict valuestr
+)
+{
+	assert(l != NULL);
+
+	if (!iniString_init(&l->id, idstr, -1))
+	{
+		return false;
+	}
+	
+	char id1[MAX_ID], id2[MAX_ID];
+	if (sscanf_s(valuestr, "%s, %s", id1, MAX_ID, id2, MAX_ID) < 2)
+	{
+		iniString_destroy(&l->id);
+		return false;
+	}
+
+	const hashNode_t * n1, * n2;
+	n1 = hashMap_get(pointmap, id1);
+	n2 = hashMap_get(pointmap, id2);
+	if ((n1 == NULL) || (n2 == NULL))
+	{
+		iniString_destroy(&l->id);
+		return false;
+	}
+
+	l->src = n1->value;
+	l->dst = n2->value;
+
+	return true;
+}
+void line_destroy(line_t * restrict l)
+{
+	assert(l != NULL);
+	iniString_destroy(&l->id);
+}
 
 dmErr_t dm_initDataFile(dataModel_t * restrict dm, const char * restrict filename)
 {
 	assert(dm != NULL);
 	assert(filename != NULL);
+
+	// Räsitabeli koostamine punktidest
+	if (!hashMap_init(&dm->ristmikud, 1))
+	{
+		return dmeMEM;
+	}
 
 	ini_t inifile;
 	if (ini_initFile(filename, &inifile) != inieOK)
@@ -46,6 +115,27 @@ dmErr_t dm_initDataFile(dataModel_t * restrict dm, const char * restrict filenam
 		return dmeSECTIONS;
 	}
 
+	// Ristmikud
+	for (size_t i = 0; i < ristmikud->numValues; ++i)
+	{
+		iniValue_t * val = ristmikud->values[i];
+		if (val != NULL)
+		{
+			point_t * p = point_makeStr(val->key.str, val->value.str);
+			// Punkti p id räsitabelisse lisamine
+			if ((p == NULL) || !hashMap_insert(&dm->ristmikud, p->id.str, p))
+			{
+				if (p != NULL)
+				{
+					point_free(p);
+				}
+				ini_destroy(&inifile);
+				dm_destroy(dm);
+				return dmeMEM;
+			}
+		}
+	}
+
 
 	// Peatused
 
@@ -55,7 +145,7 @@ dmErr_t dm_initDataFile(dataModel_t * restrict dm, const char * restrict filenam
 		numStops += (peatused->values[i] != NULL);
 	}
 	// Peatuste lisamine
-	if (numStops > (MAX_POINTS + 2))
+	if (numStops > (MAX_MID_POINTS + 2))
 	{
 		ini_destroy(&inifile);
 		return dmeSTOPS_LIMIT;
@@ -63,7 +153,6 @@ dmErr_t dm_initDataFile(dataModel_t * restrict dm, const char * restrict filenam
 
 
 	dm->numMidPoints = 0;
-
 	for (size_t i = 0, realStops = 0; i < peatused->numValues; ++i)
 	{
 		iniValue_t * val = peatused->values[i];
@@ -73,20 +162,18 @@ dmErr_t dm_initDataFile(dataModel_t * restrict dm, const char * restrict filenam
 			if (!point_initStr(&p, val->key.str, val->value.str))
 			{
 				ini_destroy(&inifile);
+				dm_destroy(dm);
 				return dmeMEM;
 			}
-			else if (realStops == 0)
-			{
-				dm->beg = p;
-			}
-			else if ((realStops + 1) == numStops)
-			{
-				dm->end = p;
-			}
-			else
+
+			if ((realStops > 0) && ((realStops + 1) < numStops))
 			{
 				dm->mid[dm->numMidPoints] = p;
 				++dm->numMidPoints;
+			}
+			else
+			{
+				dm->points[(realStops > 0)] = p;
 			}
 
 			++realStops;
@@ -96,6 +183,8 @@ dmErr_t dm_initDataFile(dataModel_t * restrict dm, const char * restrict filenam
 
 	// Mälu vabastamine, kui kõik on õnnestunud siiamaani
 	ini_destroy(&inifile);
+
+
 
 	return dmeOK;
 }
@@ -108,5 +197,16 @@ void dm_destroy(dataModel_t * restrict dm)
 	{
 		point_destroy(&dm->mid[i]);
 	}
+
+	for (size_t i = 0; i < dm->ristmikud.numNodes; ++i)
+	{
+		hashNode_t * node = dm->ristmikud.nodes[i];
+		if (node != NULL)
+		{
+			point_free(node->value);
+			node->value = NULL;
+		}
+	}
+	hashMap_destroy(&dm->ristmikud);
 }
 
--- src/dataModel.h
+++ src/dataModel.h
@@ -2,9 +2,12 @@
 #define DATAMODEL_H
 
 #include "iniFile.h"
+#include "hashmap.h"
 
 #include <stdbool.h>
 #include <stdint.h>
+
+#define MAX_ID 256
 
 
 typedef struct point
@@ -15,16 +18,45 @@ typedef struct point
 } point_t;
 
 bool point_initStr(point_t * restrict p, const char * restrict idstr, const char * restrict valuestr);
+point_t * point_makeStr(const char * restrict idstr, const char * restrict valuestr);
 void point_destroy(point_t * restrict p);
+void point_free(point_t * restrict p);
 
 
-#define MAX_POINTS 10
+typedef struct line
+{
+	iniString_t id;
+
+	const point_t * src, * dst;
+
+} line_t;
+
+bool line_initStr(
+	line_t * restrict l,
+	const hashMap_t * restrict pointmap,
+	const char * restrict idstr,
+	const char * restrict valuestr
+);
+void line_destroy(line_t * restrict l);
+
+
+#define MAX_MID_POINTS 10
+#define TOTAL_POINTS 12
 
 typedef struct dataModel
 {
-	point_t beg, end;
-	point_t mid[MAX_POINTS];
+	union
+	{
+		struct
+		{
+			point_t beg, end;
+			point_t mid[MAX_MID_POINTS];
+		};
+		point_t points[TOTAL_POINTS];
+	};
 	size_t numMidPoints;
+
+	hashMap_t ristmikud;
 
 
 } dataModel_t;
