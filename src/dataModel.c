93c91eeabfc4313ed69217f71690db251eadbaf6
makuke1234 <makuke1234@gmail.com>
2022-03-09 22:25:07 +0200

    Koordinaadi "parser"

--- src/dataModel.c
+++ src/dataModel.c
@@ -1,7 +1,41 @@
 #include "dataModel.h"
 
+#include <stdlib.h>
 
-bool dm_readData(const char * restrict filename, dataModel_t * restrict dm)
+bool point_initStr(point_t * restrict p, const char * restrict idstr, const char * restrict valuestr)
 {
+	assert(p != NULL);
+
+	if (!iniString_init(&p->id, idstr, -1))
+	{
+		return false;
+	}
+
+	char * next = NULL;
+	p->x = strtof(valuestr, &next);
+	p->y = strtof(next, NULL);
+
+	return true;
+}
+void point_destroy(point_t * restrict p)
+{
+	assert(p != NULL);
+	iniString_destroy(&p->id);
+}
+
+
+bool dm_readData(dataModel_t * restrict dm, const char * restrict filename)
+{
+	ini_t inifile;
+	if (!ini_initFile(filename, &inifile))
+	{
+		return false;
+	}
+
+
+
+
+	ini_destroy(&inifile);
+
 	return true;
 }
--- src/dataModel.h
+++ src/dataModel.h
@@ -1,13 +1,22 @@
 #ifndef DATAMODEL_H
 #define DATAMODEL_H
 
+#include "iniFile.h"
+
 #include <stdbool.h>
 #include <stdint.h>
 
+
 typedef struct point
 {
+	iniString_t id;
 	float x, y;
+
 } point_t;
+
+bool point_initStr(point_t * restrict p, const char * restrict idstr, const char * restrict valuestr);
+void point_destroy(point_t * restrict p);
+
 
 #define MAX_POINTS 10
 
@@ -20,7 +29,8 @@ typedef struct dataModel
 
 } dataModel_t;
 
-bool dm_readData(const char * restrict filename, dataModel_t * restrict dm);
+
+bool dm_readData(dataModel_t * restrict dm, const char * restrict filename);
 
 
 #endif
