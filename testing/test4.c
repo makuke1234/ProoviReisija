d770bf0ae730e949cd4672a1ec5f632e176fc49d
makuke1234 <makuke1234@gmail.com>
2022-03-09 23:18:52 +0200

    Testi uuendus

--- testing/test4.c
+++ testing/test4.c
@@ -1,16 +1,20 @@
 #include "test.h"
 #include "../src/dataModel.h"
-
+#include "../src/logger.h"
 
 int main(void)
 {
+	initLogger();
+
 	setlib("dataModel");
 
 	dataModel_t dm;
 
 	test(dm_initDataFile(&dm, "test.ini") == dmeOK, "Data reading failed!");
 
-	teststr(dm.beginning.id.str, "p0");
+	teststr(dm.beg.id.str, "p0");
+	teststr(dm.end.id.str, "p1");
+	test(dm.numMidPoints == 0, "%zu middle points exist!", dm.numMidPoints);
 
 	dm_destroy(&dm);
 
