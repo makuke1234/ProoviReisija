143c5e6a38f447c57ddddb446659ae1061358dbe
makuke1234 <makuke1234@gmail.com>
2022-03-11 10:01:15 +0200

    Kontroll

--- testing/test4.c
+++ testing/test4.c
@@ -17,6 +17,21 @@ int main(void)
 	teststr(dm.end.id.str, "p1");
 	test(dm.numMidPoints == 0, "%zu middle points exist!", dm.numMidPoints);
 
+	// Väljastab vahepunktid
+	for (size_t i = 0; i < dm.numTeed; ++i)
+	{
+		if (dm.teed[i] != NULL)
+		{
+			line_t * tee = dm.teed[i];
+			printf(
+				"Tee %s: %s: (%f, %f) -> %s: (%f, %f)\n",
+				tee->id.str,
+				tee->src->id.str, tee->src->x, tee->src->y,
+				tee->dst->id.str, tee->dst->x, tee->dst->y
+			);
+		}
+	}
+
 	dm_destroy(&dm);
 
 	return 0;
