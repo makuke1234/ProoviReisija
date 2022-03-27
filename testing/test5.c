c7f4ab5655b04b41930b42fdd671bd467f14f4a6
makuke1234 <makuke1234@gmail.com>
2022-03-27 19:23:17 +0300

    Fibonacci kuhja test

---
+++ testing/test5.c
@@ -0,0 +1,40 @@
+#include "test.h"
+#include "../src/priorityQ.h"
+
+#include <stdbool.h>
+#include <math.h>
+
+bool isclose(float a, float b, float epsilon)
+{
+	return fabsf(a - b) < epsilon;
+}
+
+void testidx(size_t idx, size_t expectedIdx)
+{
+	test(idx == expectedIdx, "Expected index %zu, got %zu", expectedIdx, idx);
+}
+
+int main(void)
+{
+	setlib("Fibonacci heap");
+	
+	pq_t q;
+	pq_init(&q);
+
+	// Insert 4, 3, 0, 2
+
+	pq_pushWithPriority(&q, 0, 4.0f);
+	pq_pushWithPriority(&q, 1, 3.0f);
+	pq_pushWithPriority(&q, 2, 0.0f);
+	pq_pushWithPriority(&q, 3, 2.0f);
+
+	testidx(pq_extractMin(&q), 2);
+	testidx(pq_extractMin(&q), 3);
+	testidx(pq_extractMin(&q), 1);
+	testidx(pq_extractMin(&q), 0);
+	testidx(pq_extractMin(&q), SIZE_MAX);
+
+	pq_destroy(&q);
+
+	return 0;
+}
