c008ee4d83fb9ec75fe63684428c7d98c85fda5a
makuke1234 <makuke1234@gmail.com>
2022-03-29 15:32:22 +0300

    Dokumenteerimine

--- src/priorityQ.h
+++ src/priorityQ.h
@@ -15,9 +15,18 @@
 #define MARKED 1
 #define NOT_MARKED 0
 
+// Reciprocal value of logarithm in base 2 of the golden ratio
 #define LOG2_GR_REC 1.4404829720657015340797013126062
 
-
+/**
+ * @brief Data structure that holds doubly linked list of tree nodes'
+ * siblings and also a pointer to it's parent and child node.
+ * Contains member 'key' which denotes the priority of the node, and 'idx'
+ * which denotes the identifier of the node.
+ * Also holds a boolean value whether the node is 'marked' or not + node's degree
+ * which is it's number of children.
+ * 
+ */
 typedef struct fibNode
 {
 	// Data
@@ -35,6 +44,14 @@ typedef struct fibNode
 
 } fibNode_t;
 
+/**
+ * @brief Data structure that holds the number of nodes in Fibonacci heap
+ * root tree and a pointer to the minimum root node.
+ * 
+ * Also holds number of items in look-up-table of all nodes, and the look-up-table
+ * itself, which holds pointers to said nodes with a particular 'idx'.
+ * 
+ */
 typedef struct fibHeap
 {
 	size_t n;
@@ -46,14 +63,68 @@ typedef struct fibHeap
 } fibHeap_t, pq_t;
 
 
+/**
+ * @brief Initializes Fibonacci heap data structure
+ * 
+ * @param q Pointer to fibHeap_t structure
+ */
 void pq_init(fibHeap_t * q);
 
+/**
+ * @brief Checks whether the heap is empty.
+ * Complexity: O(1).
+ * 
+ * @param q Pointer to fibHeap_t structure
+ * @return true Heap is empty
+ * @return false Heap contains items
+ */
 bool pq_empty(fibHeap_t * q);
+/**
+ * @brief Pushes a new item to the heap with a particular identifier 'idx' &
+ * priority of 'distance'. 'Updates' item's priority if it already exists.
+ * Complexity: O(1) if item doesn't exist prior or new distance is smaller than or equal to previous.
+ * Complexity: O(log n) if item exist prior and distance is bigger than previous.
+ * 
+ * @param q Pointer to fibHeap_t structure
+ * @param idx Identifier of pushable node
+ * @param distance Priority/key of the new node
+ * @return true Success pushing
+ * @return false Failure
+ */
 bool pq_pushWithPriority(fibHeap_t * q, size_t idx, float distance);
+/**
+ * @brief Extracts first item's (minimum item's) identifier 'idx' from heap and also
+ * removes it.
+ * Complexity: O(log n).
+ * 
+ * @param q Pointer to fibHeap_t structure
+ * @return size_t Identifier of minimum item in the heap
+ */
 size_t pq_extractMin(fibHeap_t * q);
+/**
+ * @brief Decreases the priority of a node with identifier 'idx'. New priority must be
+ * smaller!
+ * Complexity: O(1)
+ * 
+ * @param q Pointer to fibHeap_t structure
+ * @param idx Identifier of decreaseable node
+ * @param distance New decreased priority of the node
+ */
 void pq_decPriority(fibHeap_t * q, size_t idx, float distance);
+/**
+ * @brief Prints the entire Fibonacci heap structure.
+ * Complexity: O(n)
+ * 
+ * @param q Pointer to fibHeap_t structure
+ */
 void pq_print(fibHeap_t * q);
 
+/**
+ * @brief Destroys the Fibonacci heap.
+ * Complexity: O(n)
+ * 
+ * @param q Pointer to fibHeap_t structure
+ */
 void pq_destroy(fibHeap_t * q);
 
 #endif
