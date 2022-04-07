f84d09c697e325381e5312fc0e5ef4a4d716b73a
makuke1234 <makuke1234@gmail.com>
2022-04-07 15:44:43 +0300

    Algeline SVG kirjutaja

--- src/svgWriter.c
+++ src/svgWriter.c
@@ -1,2 +1,98 @@
 #include "svgWriter.h"
 
+struct svgSettings
+{
+	size_t pRadius, fontSize;
+
+};
+
+static struct svgSettings svgSettings;
+
+
+svgRGB_t svg_rgb(uint8_t r, uint8_t g, uint8_t b)
+{
+	return (svgRGB_t){
+		.r = r,
+		.g = g,
+		.b = b,
+		.a = 0xFF
+	};
+}
+svgRGB_t svg_rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+	return (svgRGB_t){
+		.r = r,
+		.g = g,
+		.b = b,
+		.a = a
+	};
+}
+svgRGB_t svg_rgba32(uint32_t color)
+{
+	return (svgRGB_t){
+		.r = (color >> 24) & 0xFF,
+		.g = (color >> 16) & 0xFF,
+		.b = (color >>  8) & 0xFF,
+		.a =  color        & 0xFF
+	};
+}
+
+
+void svg_init()
+{
+	svgSettings = (struct svgSettings){
+		.pRadius = 2,
+		.fontSize = 20
+	};
+}
+void svg_setPointRadius(size_t radius)
+{
+	svgSettings.pRadius = radius;
+}
+void svg_setFontSize(size_t sz)
+{
+	svgSettings.fontSize = sz;
+}
+
+bool svg_header(FILE * restrict fp, size_t width, size_t height, svgRGB_t backColor)
+{
+	return fprintf(
+		fp,
+		"<svg version=\"1.1\" width=\"%zu\" height=\"%zu\" xmlns=\"http://www.w3.org/2000/svg\">\n"
+		"<rect width=\"100%%\" height=\"100%%\" fill=\"rgb(%hu, %hu, %hu)\" />\n\n",
+		width,
+		height,
+		(uint16_t)backColor.r, (uint16_t)backColor.g, (uint16_t)backColor.b
+	) > 0;
+}
+
+bool svg_line(FILE * restrict fp, const line_t * restrict l, svgRGB_t color)
+{
+	return fprintf(
+		fp,
+		"<"
+	);
+}
+bool svg_linePoint(FILE * restrict fp, const line_t * restrict l, svgRGB_t color, bool drawSrc)
+{
+	return svg_line(fp, l, color) && ((drawSrc && svg_point(fp, l->src, color)) || !drawSrc) && svg_point(fp, l->dst, color);
+}
+bool svg_point(FILE * restrict fp, const point_t * restrict p, svgRGB_t color)
+{
+	return fprintf(
+		fp,
+		"<circle cx=\"%zu\" cy=\"%zu\" r=\"%zu\" fill=\"rgb(%hu, %hu, %hu)\" />\n"
+		"<text cx=\"%zu\" cy=\"%zu\" font-size=\"%zu\" text-anchor=\"\" fill=\"black\">%s</text>\n",
+		p->x, p->y,
+		svgSettings.pRadius,
+		(uint16_t)color.r, (uint16_t)color.g, (uint16_t)color.b,
+		p->x, p->y,
+		svgSettings.fontSize,
+		p->id.str
+	);
+}
+
+bool svg_footer(FILE * restrict fp)
+{
+	return fputs("\n</svg>\n", fp) > 0;
+}
--- src/svgWriter.h
+++ src/svgWriter.h
@@ -1,7 +1,115 @@
 #ifndef SVG_WRITER_H
 #define SVG_WRITER_H
 
+#include "dataModel.h"
 
+#include <stdio.h>
 
+/**
+ * @brief Data structure for RGB colors in SVG file
+ * 
+ */
+typedef struct svgRGB
+{
+	uint8_t r, g, b, a;
+
+} svgRGB_t;
+
+/**
+ * @brief Creates a svgRGB structure from 3 color components, alpha is max
+ * 
+ * @param r Red component
+ * @param g Green component
+ * @param b Blue component
+ * @return svgRGB_t svgRGB structure
+ */
+svgRGB_t svg_rgb(uint8_t r, uint8_t g, uint8_t b);
+/**
+ * @brief Creates a svgRGB structure from 3 color compoenent + alpha
+ * 
+ * @param r Red component
+ * @param g Green component
+ * @param b Blue component
+ * @param a Alpha component
+ * @return svgRGB_t svgRGB structure
+ */
+svgRGB_t svg_rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+/**
+ * @brief Creates a svgRGB structure from single 32-bit unsigned integer
+ * representing RGB hex code
+ * 
+ * @param color RGB hex code
+ * @return svgRGB_t svgRGB structure
+ */
+svgRGB_t svg_rgba32(uint32_t color);
+
+/**
+ * @brief Initialises necessary settings for SVG to work
+ * 
+ */
+void svg_init();
+/**
+ * @brief Sets the point size to draw into the SVG
+ * 
+ * @param radius Points radius
+ */
+void svg_setPointRadius(size_t radius);
+/**
+ * @brief Sets the font size to draw into the SVG
+ * 
+ * @param sz Font size
+ */
+void svg_setFontSize(size_t sz);
+
+/**
+ * @brief Writes the SVG file header to file.
+ * 
+ * @param fp output file pointer
+ * @param width Width of the image
+ * @param height Height of the image
+ * @return true Success
+ * @return false Failure
+ */
+bool svg_header(FILE * restrict fp, size_t width, size_t height);
+
+/**
+ * @brief Draws just a line in the SVG file
+ * 
+ * @param fp output file pointer
+ * @param l 
+ * @param color The RGB color of the line
+ * @return true Success
+ * @return false Failure
+ */
+bool svg_line(FILE * restrict fp, const line_t * restrict l, svgRGB_t color);
+/**
+ * @brief 
+ * 
+ * @param fp output file pointer
+ * @param l 
+ * @param color The RGB color of the line & the point
+ * @return true Success
+ * @return false Failure
+ */
+bool svg_linePoint(FILE * restrict fp, const line_t * restrict l, svgRGB_t color);
+/**
+ * @brief 
+ * 
+ * @param fp output file pointer
+ * @param p 
+ * @param color The RGB color of the point
+ * @return true Success
+ * @return false Failure
+ */
+bool svg_point(FILE * restrict fp, const point_t * restrict p, svgRGB_t color);
+
+/**
+ * @brief Writes the SVG file footer to file.
+ * 
+ * @param fp output file pointer
+ * @return true Success
+ * @return false Failure
+ */
+bool svg_footer(FILE * restrict fp);
 
 #endif
